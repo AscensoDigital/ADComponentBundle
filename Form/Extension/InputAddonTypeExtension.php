0c6a689c686721b56142ad21a45644ed72f9425c
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-02-02 16:26:25 -0300

    se define que siempre este disponible el pre y post addon

--- Form/Extension/InputAddonTypeExtension.php
+++ Form/Extension/InputAddonTypeExtension.php
@@ -44,11 +44,7 @@ class InputAddonTypeExtension extends AbstractTypeExtension {
      * @param array $options
      */
     public function buildView(FormView $view, FormInterface $form, array $options) {
-        if (array_key_exists('pre_addon', $options)) {
-            $view->vars['pre_addon'] = $options['pre_addon'];
-        }
-        if (array_key_exists('post_addon', $options)) {
-            $view->vars['post_addon'] = $options['post_addon'];
-        }
+        $view->vars['pre_addon'] = isset($options['pre_addon']) ? $options['pre_addon'] : null;
+        $view->vars['post_addon'] = isset($options['post_addon']) ? $options['post_addon'] : null;
     }
 }
