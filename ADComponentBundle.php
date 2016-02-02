92a855214ebc8e8b3df3cf2f4666a99fccccea17
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-02-02 12:49:10 -0300

    se agrega clase para definicion de bundle

---
+++ ADComponentBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudio
+ * Date: 02-02-16
+ * Time: 12:47
+ */
+
+namespace AscensoDigital\ComponentBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ADComponentBundle extends Bundle
+{
+}
