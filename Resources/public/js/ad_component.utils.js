f17ee637db6c5a867ed5f05eddcdc752d6536c94
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-06-03 23:40:54 -0400

    se agrega utilitario de javascript

---
+++ Resources/public/js/ad_component.utils.js
@@ -0,0 +1,8 @@
+/**
+ * Created by claudio on 04-06-16.
+ */
+var ADComponent = {
+    msgAlert : function (status, msg) {
+          return '<div class="alert alert-'+ status +' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> '+ msg + '</div>';
+    };
+};
