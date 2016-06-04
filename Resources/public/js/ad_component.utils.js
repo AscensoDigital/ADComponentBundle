04187a28a6c279832e0b3d4316beeba28db47c9b
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-06-03 23:54:53 -0400

    se agrega metodo msgLabel

--- Resources/public/js/ad_component.utils.js
+++ Resources/public/js/ad_component.utils.js
@@ -2,7 +2,12 @@
  * Created by claudio on 04-06-16.
  */
 var ADComponent = {
-    msgAlert : function (status, msg) {
-          return '<div class="alert alert-'+ status +' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> '+ msg + '</div>';
+    msgAlert : function (level, msg) {
+          return '<div class="alert alert-'+ level +' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> '+ msg + '</div>';
+    },
+
+    msgLabel: function (label, level, msg) {
+        var label = "label label-" + level;
+        return "<span class='" + label + "'>" + label + "</span><br><div>" + msg + "</div>";
     },
 };
