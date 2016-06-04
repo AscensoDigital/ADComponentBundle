e79cf0b351093cb6e94f2ce5392138dce3c38b22
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-06-03 23:46:33 -0400

    se modifica ; por ,

--- Resources/public/js/ad_component.utils.js
+++ Resources/public/js/ad_component.utils.js
@@ -4,5 +4,5 @@
 var ADComponent = {
     msgAlert : function (status, msg) {
           return '<div class="alert alert-'+ status +' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> '+ msg + '</div>';
-    };
+    },
 };
