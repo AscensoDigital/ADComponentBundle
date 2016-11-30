1d6e9be2c605f21ca8a61d83e6917b26a53b43c8
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-11-30 02:57:18 -0300

    se agrega listener para actualizar modificador de tablas con dicho campo

---
+++ EventListener/RegisterModificadorSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AscensoDigital\ComponentBundle\EventListener;
+
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+/**
+ * Description of RegisterModificadorSubscriber
+ *
+ * @author claudio
+ */
+class RegisterModificadorSubscriber implements EventSubscriber {
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array('prePersist', 'preUpdate');
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $this->index($args);
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if(method_exists($entity, 'setCreatedAt')){
+            call_user_func(array($entity, 'setCreatedAt'), new \DateTime());
+        }
+        $this->index($args);
+    }
+
+    public function index(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $usuario= is_null($this->tokenStorage->getToken()) ? null : $this->tokenStorage->getToken()->getUser();
+        if(method_exists($entity, 'setModificador')) {
+            call_user_func(array($entity, 'setModificador'), is_object($usuario) ? $usuario : null);
+        }
+        if(method_exists($entity, 'setModificadorId')){
+            call_user_func(array($entity, 'setModificadorId'), is_null($usuario) ? null : $usuario->getId());
+        }
+        if(method_exists($entity, 'setUpdatedAt')) {
+            call_user_func(array($entity, 'setUpdatedAt'), new \DateTime());
+        }
+    }
+}
