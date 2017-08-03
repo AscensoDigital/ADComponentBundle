aec69f6b3c6b9153c7473357589dced89ab6276d
claudiocorvalan <corvalanclaudio7@gmail.com>
2017-08-02 23:28:05 -0400

    se corrige manejo de icono variable

--- Resources/public/js/ad_component.jq.rating.js
+++ Resources/public/js/ad_component.jq.rating.js
@@ -8,16 +8,18 @@ $(document).ready(function(){
         var label=$(this).data('rating-label');
         var inputHidden = $(this).parent('span').children("input[type='hidden']")[0];
         var ratingLabel = $(this).parent('span').children('.ad_component-rating-label')[0];
+        var iconBase = $(this).parent('span').data('icon-base');
+        var iconCheck = $(this).parent('span').data('icon-check');
 
         $(inputHidden).val(valor);
         $(ratingLabel).html(label);
 
         $(this).parent("span").children("i").each(function(){
             if(parseInt($(this).data('rating'))<= valor ){
-                $(this).removeClass('fa-star-o').addClass('fa-star');
+                $(this).removeClass(iconBase).addClass(iconCheck);
             }
             else {
-                $(this).removeClass('fa-star').addClass('fa-star-o');
+                $(this).removeClass(iconCheck).addClass(iconBase);
             }
         });
     });
@@ -26,15 +28,17 @@ $(document).ready(function(){
         var valor=parseInt($(this).data('rating'));
         var label=$(this).data('rating-label');
         var ratingLabel = $(this).parent('span').children('.ad_component-rating-label')[0];
+        var iconBase = $(this).parent('span').data('icon-base');
+        var iconCheck = $(this).parent('span').data('icon-check');
 
         $(ratingLabel).html(label);
 
         $(this).parent("span").children("i").each(function(){
             if(parseInt($(this).data('rating'))<= valor ){
-                $(this).removeClass('fa-star-o').addClass('fa-star');
+                $(this).removeClass(iconBase).addClass(iconCheck);
             }
             else {
-                $(this).removeClass('fa-star').addClass('fa-star-o');
+                $(this).removeClass(iconCheck).addClass(iconBase);
             }
         });
     });
@@ -43,17 +47,19 @@ $(document).ready(function(){
         var inputHidden = $(this).parent('span').children("input[type='hidden']")[0];
         var ratingLabel = $(this).parent('span').children('.ad_component-rating-label')[0];
         var valor = parseInt($(inputHidden).val());
+        var iconBase = $(this).parent('span').data('icon-base');
+        var iconCheck = $(this).parent('span').data('icon-check');
 
         $(this).parent("span").children("i").each(function(){
             var rating_value= parseInt($(this).data('rating'));
             if(rating_value <= valor ){
-                $(this).removeClass('fa-star-o').addClass('fa-star');
+                $(this).removeClass(iconBase).addClass(iconCheck);
                 if(rating_value === valor){
                     $(ratingLabel).html($(this).data('rating-label'));
                 }
             }
             else {
-                $(this).removeClass('fa-star').addClass('fa-star-o');
+                $(this).removeClass(iconCheck).addClass(iconBase);
             }
         });
     });
