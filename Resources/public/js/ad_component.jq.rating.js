8f37352be59c401d354083850034c942c3af5f49
claudiocorvalan <corvalanclaudio7@gmail.com>
2017-08-03 11:34:47 -0400

    se corrige inicializacion con valor nulo de rating

--- Resources/public/js/ad_component.jq.rating.js
+++ Resources/public/js/ad_component.jq.rating.js
@@ -50,9 +50,8 @@ $(document).ready(function(){
         var iconBase = $(this).parent('span').data('icon-base');
         var iconCheck = $(this).parent('span').data('icon-check');
 
-        if(valor === NaN){
-            $(ratingLabel).html('');
-        }
+        $(ratingLabel).html('');
+
         $(this).parent("span").children("i").each(function(){
             var rating_value= parseInt($(this).data('rating'));
             if(rating_value <= valor ){
