7e34ec514450498828022c6bc07ec6c665a7efc3
Claudio Corvalan <chiko6669@gmail.com>
2021-02-22 19:51:23 -0300

    se implementa funcion generadora de Password

--- Util/StrUtil.php
+++ Util/StrUtil.php
@@ -46,6 +46,26 @@ class StrUtil
         return str_replace('&quot;','"',utf8_decode(str_replace('À',';',$contenido)));
     }
 
+    function generatePassword($longitud = 8, $opcLetra = TRUE, $opcNumero = TRUE, $opcMayus = TRUE, $opcEspecial = TRUE){
+        $letras ="abcdefghijklmnopqrstuvwxyz";
+        $numeros = "1234567890";
+        $letrasMayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $especiales ="|@#$%()=^*+[]{}-_";
+        $listado = "";
+        $password = "";
+        if ($opcLetra == TRUE) $listado .= $letras;
+        if ($opcNumero == TRUE) $listado .= $numeros;
+        if ($opcMayus == TRUE) $listado .= $letrasMayus;
+        if ($opcEspecial == TRUE) $listado .= $especiales;
+        str_shuffle($listado);
+        for( $i=1; $i<=$longitud; $i++) {
+            $caracter = $listado[rand(0,strlen($listado)-1)];
+            $password.=$caracter;
+            $listado = str_shuffle($listado);
+        }
+        return $password;
+    }
+
     /**
      * Reemplaza todos los acentos por sus equivalentes sin ellos
      * @param $string
