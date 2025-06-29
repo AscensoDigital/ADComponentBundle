6c0306cb93ecbc5e02b232f941710413faa36760
Claudio Corvalan <chiko6669@gmail.com>
2025-06-28 20:49:04 -0400

    se implementa entorno de pruebas para archivos js

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            // Cargar como script puro
+            {
+                pattern: 'Resources/public/js/ad_component.utils.js',
+                included: true,
+                served: true,
+                watched: false
+            },
+            // Archivos de prueba (sí usan Webpack)
+            {
+                pattern: 'tests/js/**/*.spec.js',
+                included: true
+            }
+        ],
+        preprocessors: {
+            'Resources/public/js/ad_component.utils.js': ['coverage'],
+            'tests/js/**/*.spec.js': ['webpack', 'sourcemap']
+        },
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-webpack',
+            'karma-sourcemap-loader',
+            'karma-coverage'
+        ],
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/',
+            subdir: '.',
+            includeAllSources: true
+        },
+        webpack: {
+            mode: 'development',
+            devtool: 'inline-source-map'
+        },
+        browsers: ['ChromeHeadless'],
+        reporters: ['progress', 'coverage'],
+        singleRun: true
+    });
+};
